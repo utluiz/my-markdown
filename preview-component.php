18a00cd38d602f468f7213406a75ab7c5fe9eb62
Luiz Silva <lsilva@atlassian.com>
2016-04-05 19:28:39 +1000

    project structure organization readme big update licensed checked

--- preview-component.php
+++ preview-component.php
@@ -1,10 +1,15 @@
 <template id="mmd-preview-template">
     <style>
         @import url("<?php echo plugin_dir_url(__FILE__); ?>css/editor-preview.css");
+<?php if (file_exists(wp_get_theme()->get_template_directory() . '/mmd-preview-editor.css')) { ?>
         @import url("<?php echo wp_get_theme()->get_template_directory_uri(); ?>/mmd-preview-editor.css");
+<?php } ?>
     </style>
     <div class='mmd-preview-content prettyprint'></div>
 </template>
+<?php if (file_exists(wp_get_theme()->get_template_directory() . '/mmd-preview-editor.js')) { ?>
+<script type="text/java" src=""<?php echo wp_get_theme()->get_template_directory_uri(); ?>/mmd-preview-editor.js""></script>
+<?php } ?>
 <script>
     var mmd_template = document.currentScript.ownerDocument.querySelector('#mmd-preview-template');
     var mmdPreview = document.registerElement('mmd-preview', {
